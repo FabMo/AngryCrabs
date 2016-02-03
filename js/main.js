5325bdce49ebbc989d72dd100e63c651ed9d122f
Brendan Collins <brendan.c.collins@gmail.com>
2016-02-03 12:42:03 -0500

    adding splash page

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-$( document ).ready(function() {
-	$('.advanced').hide();
-	fabmoDashboard.getConfig(function(err, data) {
-      if(err) {
-        console.log(err);
-      } 
-	});  
+$(document).ready(function() {
+  $('.advanced').hide();
+  fabmoDashboard.getConfig(function(err, data) {
+    if (err) {
+      console.log(err);
+    }
+  });
 });
 
-  // Object to store the App configuration that we'll read from fabmo
+// Object to store the App configuration that we'll read from fabmo
 var appConfig = {};
 
 // $( document ).ready(function() {
@@ -26,7 +26,7 @@ var appConfig = {};
 //             console.info('Key "' + key + '" found in the app config with a value of ' + data[key]);
 //             $('#' + key).val(appConfig[key])
 //         }
-		
+
 // 		 // For Bill:
 //         appConfig.timesAppWasLoaded = appConfig.timesAppWasLoaded ||  0;
 //         appConfig.timesAppWasLoaded++;
@@ -43,128 +43,117 @@ var appConfig = {};
 var gamelevel = parseInt(localStorage.getItem('gamelevel')) || 0;
 
 $('form').parsley().on('field:success', function() {
-    // This event will fire whenever a field successfully validates.
-    // 'this' will be a ParsleyField instance
+  // This event will fire whenever a field successfully validates.
+  // 'this' will be a ParsleyField instance
 
-    var el = this.$element;             // Get the jquery element from the ParsleyField instance
-    var id = el.attr('id');             // Get the id from the jquery element
-    
-    // Update the saved app config with the validated value we just retrieved
-    appConfig[id] = el.val();
+  var el = this.$element; // Get the jquery element from the ParsleyField instance
+  var id = el.attr('id'); // Get the id from the jquery element
 
-    // Send the config back to the tool
-    console.info("Sending app config: " + JSON.stringify(appConfig));
-    fabmoDashboard.setAppConfig(appConfig);
+  // Update the saved app config with the validated value we just retrieved
+  appConfig[id] = el.val();
+
+  // Send the config back to the tool
+  console.info("Sending app config: " + JSON.stringify(appConfig));
+  fabmoDashboard.setAppConfig(appConfig);
 });
 
-$('.exit-modal').on('click', function (){
-	$('.modal, .modal-container').fadeOut('fast');
+$('.exit-modal').on('click', function() {
+  $('.modal, .modal-container').fadeOut('fast');
+  gamelevel = 1;
+  levelChecker(gamelevel);
 });
 
 
 
-$('#submit').click(function (e){
-	e.preventDefault();
-	e.stopPropagation();
-	if ( gamelevel < 4){
-	gamelevel++;
-	} else {
-		 gamelevel= 1
-	}
- 	levelChecker(gamelevel);
-	localStorage.setItem('gamelevel', gamelevel);
+$('#submit').click(function(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  if (gamelevel < 4) {
+    gamelevel++;
+  } else {
+    gamelevel = 1
+  }
+  levelChecker(gamelevel);
+  localStorage.setItem('gamelevel', gamelevel);
 });
 gamelevel = 0;
 
-var levelChecker = function (level) {
+var levelChecker = function(level) {
 
-		switch (level) {
-			
-		case 0:
-		game_intro.innerHTML = " Angry Crabs is an exploration of an alternate app interface that enables additional interface options as the user gains ";
-		game_intro.innerHTML = game_intro.innerHTML + "experience with cutting. In the beginning most of the options are set to defaults, but each time the app is opened ";
-		game_intro.innerHTML = game_intro.innerHTML + "new options are added. <br> <br>Click the DONE button to move to the next level in this demo, but in a real app you would be cutting. <br><br>";
-		
-	gameplay1.innerHTML = "";
-		gameplay2.innerHTML = "";
-		gameplay3.innerHTML = "";
-		gameplay4.innerHTML = "";
-		$('#level1tab').hide();
-		$('#level2tab').hide();
-		$('#level3tab').hide();
-		$('#level4tab').hide();
-		$('.LevelD').hide();
-		$('.Level1').hide();
-		$('.Level2').hide();
-		$('.Level3').hide();
-		$('.Level4').hide();	
-		break;
-			
-	    case 1: 
-		game_intro.innerHTML = "";	
-		gameplay1.innerHTML = "<br>You are a lowly crab larvae and can only set the diameter and depth of the hole. With experience though you'll soon have more crabby skills!  <br><br>";
-		gameplay2.innerHTML = "";
-		gameplay3.innerHTML = "";
-		gameplay4.innerHTML = "";
-		$('#level1tab').show();
-		$('#level2tab').hide();
-		$('#level3tab').hide();
-		$('#level4tab').hide();
-		$('.LevelD').show();
-		$('.Level1').show();
-		$('.Level2').hide();
-		$('.Level3').hide();
-		$('.Level4').hide();		
-		break;
-		case 2:
-		game_intro.innerHTML = "";
-		gameplay1.innerHTML = "";
-		gameplay2.innerHTML = "<br>Congratulations! You have unlocked the Peeler level and now have more options for how holes will be cut. <br><br>";
-		gameplay3.innerHTML = "";
-		gameplay4.innerHTML = "";
-		$('#level1tab').hide();
-		$('#level2tab').show();
-		$('#level3tab').hide();
-		$('#level4tab').hide();
-		$('.LevelD').show();
-		$('.Level1').show();
-		$('.Level2').show();
-		$('.Level3').hide();
-		$('.Level4').hide();
-		break
-		case 3:
-		game_intro.innerHTML = "";
-		gameplay1.innerHTML = "";
-		gameplay2.innerHTML = "";
-		gameplay3.innerHTML = "<br>Now you're a big-old Jimmy crab! You have unlocked the Hole Pincher level and can now cut oval holes if you want. <br><br>";
-		gameplay4.innerHTML = "";
-		$('#level1tab').hide();
-		$('#level2tab').hide();
-		$('#level3tab').show();
-		$('#level4tab').hide();
-		$('.Level1').show();
-		$('.Level2').show();
-		$('.Level3').show();
-		$('.Level4').hide();
-		$('.LevelD').hide();
-		break;
-		case 4:
-		game_intro.innerHTML = "";
-		gameplay1.innerHTML = "";
-		gameplay2.innerHTML = "";
-		gameplay3.innerHTML = "";
-		gameplay4.innerHTML = "<br>I'm about to become dinner, but before they cover me in Old Bay and toss me in boiling water I have one more gift for you...the power of Pocketing.  Use it wisely. <br><br>";
-		$('#level1tab').hide();
-		$('#level2tab').hide();
-		$('#level3tab').hide();
-		$('#level4tab').show();
-		$('.Level1').show();
-		$('.Level2').show();
-		$('.Level3').show();
-		$('.Level4').show();
-		$('.LevelD').hide();
-		break;
-	}
+  switch (level) {
+
+    case 0:
+      $('.modal-content p').text(' Angry Crabs is an exploration of an alternate app interface that enables additional interface options as the user gains experience with cutting. In the beginning most of the options are set to defaults, but each time the app is opened Click the OKAY button to move to the next level in this demo, but in a real app you would be cutting.');
+      $('.modal, .modal-container').fadeIn();
+      $('.settings').hide();
+      break;
+
+    case 1:
+      $('.settings').show();
+      game_intro.innerHTML = "";
+      gameplay1.innerHTML = "<br>You are a lowly crab larvae and can only set the diameter and depth of the hole. With experience though you'll soon have more crabby skills!  <br><br>";
+      gameplay2.innerHTML = "";
+      gameplay3.innerHTML = "";
+      gameplay4.innerHTML = "";
+      $('#level1tab').show();
+      $('#level2tab').hide();
+      $('#level3tab').hide();
+      $('#level4tab').hide();
+      $('.LevelD').show();
+      $('.Level1').show();
+      $('.Level2').hide();
+      $('.Level3').hide();
+      $('.Level4').hide();
+      break;
+    case 2:
+      game_intro.innerHTML = "";
+      gameplay1.innerHTML = "";
+      gameplay2.innerHTML = "<br>Congratulations! You have unlocked the Peeler level and now have more options for how holes will be cut. <br><br>";
+      gameplay3.innerHTML = "";
+      gameplay4.innerHTML = "";
+      $('#level1tab').hide();
+      $('#level2tab').show();
+      $('#level3tab').hide();
+      $('#level4tab').hide();
+      $('.LevelD').show();
+      $('.Level1').show();
+      $('.Level2').show();
+      $('.Level3').hide();
+      $('.Level4').hide();
+      break
+    case 3:
+      game_intro.innerHTML = "";
+      gameplay1.innerHTML = "";
+      gameplay2.innerHTML = "";
+      gameplay3.innerHTML = "<br>Now you're a big-old Jimmy crab! You have unlocked the Hole Pincher level and can now cut oval holes if you want. <br><br>";
+      gameplay4.innerHTML = "";
+      $('#level1tab').hide();
+      $('#level2tab').hide();
+      $('#level3tab').show();
+      $('#level4tab').hide();
+      $('.Level1').show();
+      $('.Level2').show();
+      $('.Level3').show();
+      $('.Level4').hide();
+      $('.LevelD').hide();
+      break;
+    case 4:
+      game_intro.innerHTML = "";
+      gameplay1.innerHTML = "";
+      gameplay2.innerHTML = "";
+      gameplay3.innerHTML = "";
+      gameplay4.innerHTML = "<br>I'm about to become dinner, but before they cover me in Old Bay and toss me in boiling water I have one more gift for you...the power of Pocketing.  Use it wisely. <br><br>";
+      $('#level1tab').hide();
+      $('#level2tab').hide();
+      $('#level3tab').hide();
+      $('#level4tab').show();
+      $('.Level1').show();
+      $('.Level2').show();
+      $('.Level3').show();
+      $('.Level4').show();
+      $('.LevelD').hide();
+      break;
+  }
 };
 
 levelChecker(gamelevel);
